e55e5d74da3fe2f55f667b198651c1a4520332c5
Brian Schlining <bschlining@gmail.com>
2025-01-31 16:24:38 -0800

    check point

--- src/main/scala/org/mbari/vars/migration/services/MigrateService.scala
+++ src/main/scala/org/mbari/vars/migration/services/MigrateService.scala
@@ -7,13 +7,23 @@
 
 package org.mbari.vars.migration.services
 
+import org.mbari.vars.migration.model.MediaFactory
 import vars.ToolBelt
 import vars.annotation.VideoArchive
 
-class MigrateService(toolBelt: ToolBelt):
+import java.nio.file.Path
+
+class MigrateService(toolBelt: ToolBelt, csvLookup: Path):
+
+    private val mediaFactory = new MediaFactory(csvLookup)
 
     def migrate(videoArchive: VideoArchive, missionContact: String): Unit =
         println(s"Migrating ${videoArchive.getName} with missionContact $missionContact")
         // Do the migration here
+        mediaFactory.toMedia(videoArchive) match
+            case Some(media) =>
+                println(media)
+            case None =>
+                println(s"Not able to transform ${videoArchive.getName} to a media object")
         println("Migration complete")
         println("")
